cfe7d4c6f6d88469e06d04133ad250fcf3720daf
Nasonov V <qxuken@gmail.com>
2024-03-13 01:48:05 +0300

    Removed try blocks due to unstable nature

--- packages/dictator/src/file/upload_state.rs
+++ packages/dictator/src/file/upload_state.rs
@@ -14,6 +14,175 @@ use crate::error::{AppError, Result};
 pub struct FileUploadRequested {
     storage: Arc<Storage>,
     db: Arc<Database>,
+    dir: Option<Arc<Dir>>,
+    file: Option<Arc<File>>,
+    file_version: Option<Arc<Mutex<FileVersion>>>,
+    file_path: Option<String>,
+}
+
+impl FileUploadRequested {
+    #[instrument(skip_all)]
+    pub fn new(storage: Arc<Storage>, db: Arc<Database>) -> Self {
+        Self {
+            storage,
+            db,
+            dir: None,
+            file: None,
+            file_version: None,
+            file_path: None,
+        }
+    }
+}
+
+impl FileUploadRequested {
+    #[instrument(skip_all)]
+    pub async fn cleanup(&mut self) -> Result<()> {
+        tracing::debug!("Cleaning requested");
+
+        let mut connection = self
+            .db
+            .establish_connection()
+            .await
+            .map_err(AppError::from)?;
+        let storage = self.storage.clone();
+
+        if let Some(file_version) = &self.file_version {
+            tracing::trace!("Cleaning up file_version");
+            tracing::trace!("{file_version:?}");
+            file_version
+                .lock()
+                .await
+                .unsafe_delete(&mut connection)
+                .await
+                .map_err(AppError::from)?;
+        }
+
+        if let Some(path) = &self.file_path {
+            tracing::trace!("Cleaning up system file");
+            storage.remove_file(path).await.map_err(AppError::from)?;
+        }
+
+        if let Some(file) = &self.file {
+            tracing::trace!("Cleaning up file");
+            tracing::trace!("{file:?}");
+            file.delete_if_no_versions_exists(&mut connection)
+                .await
+                .map_err(AppError::from)?;
+        }
+
+        if let Some(dir) = &self.dir {
+            tracing::trace!("Cleaning up dir");
+            tracing::trace!("{dir:?}");
+            dir.delete_if_no_files_exists(&mut connection)
+                .await
+                .map_err(AppError::from)?;
+        }
+
+        Ok(())
+    }
+
+    async fn internal_got_meta(&mut self, meta: UploadMeta) -> Result<FileUploading> {
+        let mut connection = self
+            .db
+            .establish_connection()
+            .await
+            .map_err(AppError::from)?;
+        let storage = self.storage.clone();
+
+        let dir = DirUpsert {
+            name: meta.dir.clone(),
+            created_at: None,
+        }
+        .find_by_name_or_create(&mut connection)
+        .await
+        .map(Arc::new)
+        .map_err(AppError::from)?;
+        tracing::trace!("Created dir");
+        tracing::trace!("{dir:?}");
+
+        self.dir = Some(dir.clone());
+
+        let file_upsert = FileUpsert {
+            dir_id: dir.id,
+            name: meta.name.clone(),
+            media_type: meta.media_type.clone(),
+            created_at: None,
+        };
+        let file = file_upsert
+            .find_by_name_or_create(&mut connection)
+            .await
+            .map(Arc::new)
+            .map_err(AppError::from)?;
+        tracing::trace!("Created file");
+        tracing::trace!("{file:?}");
+
+        self.file = Some(file.clone());
+
+        let file_version_upsert = NewFileVersion {
+            file_id: file.id,
+            size: meta.size,
+            hash: meta.hash.clone(),
+            name: meta.version.clone(),
+            state: FileVersionState::Downloading,
+            created_at: None,
+        };
+        let file_version = file_version_upsert
+            .create(&mut connection)
+            .await
+            .map(Mutex::new)
+            .map(Arc::new)
+            .map_err(AppError::from)?;
+        tracing::trace!("Created file version");
+        tracing::trace!("{:?}", file_version.lock().await);
+
+        self.file_version = Some(file_version.clone());
+
+        let path = file_version
+            .lock()
+            .await
+            .path(&mut connection)
+            .await
+            .map_err(AppError::from)?
+            .to_string();
+        tracing::trace!("Storage path {path:?}");
+
+        self.file_path = Some(path.clone());
+
+        let file_handle = storage.create_file(&path).await?;
+        tracing::trace!("Created system file");
+        tracing::trace!("{file_handle:?}");
+
+        file_handle
+            .set_len(meta.size.try_into().map_err(|e: TryFromIntError| {
+                AppError::Other(format!("Size conversion failed {}", e))
+            })?)
+            .await?;
+
+        Ok(FileUploading {
+            db: self.db.clone(),
+            storage,
+            received_bytes: 0,
+            meta,
+            file_handle,
+            path,
+            dir,
+            file,
+            file_version,
+        })
+    }
+
+    #[instrument(skip(self))]
+    pub async fn got_meta(mut self, meta: UploadMeta) -> Result<FileUploading> {
+        tracing::debug!("Received meta");
+
+        let res: Result<FileUploading> = self.internal_got_meta(meta).await;
+
+        if res.is_err() {
+            self.cleanup().await.ok();
+        }
+
+        res
+    }
 }
 
 pub struct FileUploading {
@@ -28,154 +197,9 @@ pub struct FileUploading {
     file_version: Arc<Mutex<FileVersion>>,
 }
 
-impl FileUploadRequested {
-    #[instrument(skip_all)]
-    pub fn new(storage: Arc<Storage>, db: Arc<Database>) -> Self {
-        Self { storage, db }
-    }
-}
-
-impl FileUploadRequested {
-    #[instrument(skip(self))]
-    pub async fn got_meta(self, meta: UploadMeta) -> Result<FileUploading> {
-        tracing::debug!("Received meta");
-        let mut cleanup_dir = None;
-        let mut cleanup_file = None;
-        let mut cleanup_file_version = None;
-        let mut cleanup_file_path = None;
-
-        let mut connection = self
-            .db
-            .establish_connection()
-            .await
-            .map_err(AppError::from)?;
-        let storage = self.storage.clone();
-
-        let res: Result<FileUploading> = try {
-            let dir = DirUpsert {
-                name: meta.dir.clone(),
-                created_at: None,
-            }
-            .find_by_name_or_create(&mut connection)
-            .await
-            .map(Arc::new)
-            .map_err(AppError::from)?;
-            tracing::trace!("Created dir");
-            tracing::trace!("{dir:?}");
-
-            cleanup_dir = Some(dir.clone());
-
-            let file_upsert = FileUpsert {
-                dir_id: dir.id,
-                name: meta.name.clone(),
-                media_type: meta.media_type.clone(),
-                created_at: None,
-            };
-            let file = file_upsert
-                .find_by_name_or_create(&mut connection)
-                .await
-                .map(Arc::new)
-                .map_err(AppError::from)?;
-            tracing::trace!("Created file");
-            tracing::trace!("{file:?}");
-
-            cleanup_file = Some(file.clone());
-
-            let file_version_upsert = NewFileVersion {
-                file_id: file.id,
-                size: meta.size,
-                hash: meta.hash.clone(),
-                name: meta.version.clone(),
-                state: FileVersionState::Downloading,
-                created_at: None,
-            };
-            let file_version = file_version_upsert
-                .create(&mut connection)
-                .await
-                .map(Mutex::new)
-                .map(Arc::new)
-                .map_err(AppError::from)?;
-            tracing::trace!("Created file version");
-            tracing::trace!("{:?}", file_version.lock().await);
-
-            cleanup_file_version = Some(file_version.clone());
-
-            let path = file_version
-                .lock()
-                .await
-                .path(&mut connection)
-                .await
-                .map_err(AppError::from)?
-                .to_string();
-            tracing::trace!("Storage path {path:?}");
-
-            cleanup_file_path = Some(path.clone());
-
-            let file_handle = storage.create_file(&path).await?;
-            tracing::trace!("Created system file");
-            tracing::trace!("{file_handle:?}");
-
-            file_handle
-                .set_len(meta.size.try_into().map_err(|e: TryFromIntError| {
-                    AppError::Other(format!("Size conversion failed {}", e))
-                })?)
-                .await?;
-
-            FileUploading {
-                storage: self.storage,
-                db: self.db,
-                received_bytes: 0,
-                meta,
-                file_handle,
-                path,
-                dir,
-                file,
-                file_version,
-            }
-        };
-
-        if res.is_err() {
-            tracing::trace!("Cleaning needed");
-            if let Some(file_version) = &cleanup_file_version {
-                tracing::trace!("Cleaning up file_version");
-                tracing::trace!("{file_version:?}");
-                file_version
-                    .lock()
-                    .await
-                    .unsafe_delete(&mut connection)
-                    .await
-                    .map_err(AppError::from)?;
-            }
-
-            if let Some(path) = cleanup_file_path {
-                tracing::trace!("Cleaning up system file");
-                storage.remove_file(&path).await.map_err(AppError::from)?;
-            }
-
-            if let Some(file) = &cleanup_file {
-                tracing::trace!("Cleaning up file");
-                tracing::trace!("{file:?}");
-                file.delete_if_no_versions_exists(&mut connection)
-                    .await
-                    .map_err(AppError::from)?;
-            }
-
-            if let Some(dir) = &cleanup_dir {
-                tracing::trace!("Cleaning up dir");
-                tracing::trace!("{dir:?}");
-                dir.delete_if_no_files_exists(&mut connection)
-                    .await
-                    .map_err(AppError::from)?;
-            }
-        }
-
-        res
-    }
-}
-
 impl FileUploading {
     #[instrument(skip_all)]
-    pub async fn cleanup(&self) -> Result<()> {
+    pub async fn cleanup(&mut self) -> Result<()> {
         tracing::debug!("Cleaning requested");
 
         let mut connection = self.db.establish_connection().await?;
@@ -190,6 +214,7 @@ impl FileUploading {
             .map_err(AppError::from)?;
 
         tracing::trace!("Cleaning up system file");
+        self.file_handle.shutdown().await?;
         self.storage
             .remove_file(&self.path)
             .await
@@ -212,72 +237,80 @@ impl FileUploading {
         Ok(())
     }
 
+    async fn internal_got_part(&mut self, part: FilePart) -> Result<()> {
+        self.received_bytes += i64::try_from(part.bytes.len()).map_err(|e: TryFromIntError| {
+            AppError::Other(format!("Size conversion failed {}", e))
+        })?;
+        self.file_handle
+            .write_all(&part.bytes)
+            .await
+            .map_err(AppError::from)?;
+        self.file_handle.flush().await.map_err(AppError::from)?;
+
+        Ok(())
+    }
+
     #[instrument(skip_all, fields(chunk = part.bytes.len()))]
     pub async fn got_part(&mut self, part: FilePart) -> Result<()> {
-        tracing::trace!("Incoming bytes {:?}", part.bytes.len());
-        let res: Result<()> = try {
-            self.received_bytes +=
-                i64::try_from(part.bytes.len()).map_err(|e: TryFromIntError| {
-                    AppError::Other(format!("Size conversion failed {}", e))
-                })?;
-            self.file_handle
-                .write_all(&part.bytes)
-                .await
-                .map_err(AppError::from)?;
-            self.file_handle.flush().await.map_err(AppError::from)?;
-        };
+        tracing::trace!("Incoming bytes");
+        let res: Result<()> = self.internal_got_part(part).await;
 
         if res.is_err() {
-            self.cleanup().await?;
+            self.cleanup().await.ok();
         }
 
         res
     }
 
+    async fn internal_end(&mut self) -> Result<UploadResponse> {
+        let mut connection = self
+            .db
+            .establish_connection()
+            .await
+            .map_err(AppError::from)?;
+        if self.meta.size != self.received_bytes {
+            tracing::debug!("Size check failed");
+            return Err(AppError::DataCorruption(format!(
+                "File transmission corrupted: {} / {} bytes received",
+                self.received_bytes, self.meta.size
+            ))
+            .into());
+        }
+
+        let path = self.storage.get_path(&self.path, false);
+        let hash = qcdn_utils::hash::sha256_file(&path)
+            .await
+            .map_err(AppError::from)?;
+        if self.meta.hash != hash {
+            tracing::debug!("Hash check failed");
+            return Err(AppError::DataCorruption(
+                "File transmission corrupted: hash mismatch".to_string(),
+            )
+            .into());
+        }
+
+        tracing::debug!("Updating version state to ready");
+        self.file_version
+            .lock()
+            .await
+            .update_state(&mut connection, FileVersionState::Ready)
+            .await
+            .map_err(AppError::from)?;
+
+        Ok(UploadResponse {
+            dir_id: self.dir.id,
+            file_id: self.file.id,
+            file_version_id: self.file_version.lock().await.id,
+        })
+    }
+
     #[instrument(skip(self))]
-    pub async fn end(self) -> Result<UploadResponse> {
+    pub async fn end(mut self) -> Result<UploadResponse> {
         tracing::debug!("Stream ended");
-        let res: Result<UploadResponse> = try {
-            let mut connection = self
-                .db
-                .establish_connection()
-                .await
-                .map_err(AppError::from)?;
-            if self.meta.size != self.received_bytes {
-                tracing::debug!("Size check failed");
-                return Err(
-                    AppError::DataCorruption("file transmission corrupted".to_string()).into(),
-                );
-            }
-
-            let path = self.storage.get_path(&self.path, false);
-            let hash = qcdn_utils::hash::sha256_file(&path)
-                .await
-                .map_err(AppError::from)?;
-            if self.meta.hash != hash {
-                tracing::debug!("Hash check failed");
-                return Err(
-                    AppError::DataCorruption("file transmission corrupted".to_string()).into(),
-                );
-            }
-
-            tracing::debug!("Updating version state to ready");
-            self.file_version
-                .lock()
-                .await
-                .update_state(&mut connection, FileVersionState::Ready)
-                .await
-                .map_err(AppError::from)?;
-
-            UploadResponse {
-                dir_id: self.dir.id,
-                file_id: self.file.id,
-                file_version_id: self.file_version.lock().await.id,
-            }
-        };
+        let res: Result<UploadResponse> = self.internal_end().await;
 
         if res.is_err() {
-            self.cleanup().await?;
+            self.cleanup().await.ok();
         }
 
         res
